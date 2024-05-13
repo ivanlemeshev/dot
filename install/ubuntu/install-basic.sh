12c8e042813c0ac6424bd0faee9c88e2e25e00fb
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-05-14 00:20:48 +0300

    Add zsh (#15)

--- install/ubuntu/install-basic.sh
+++ install/ubuntu/install-basic.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# set -e is an option that tells the shell to exit immediately if any command
+# exits with a non-zero status.
+set -e
+
 echo "--- Installing basic packages"
 
 sudo apt install -y curl wget git htop vim make unzip gcc ripgrep
