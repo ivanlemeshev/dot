87fdc1442d8b1b449d8be174a5684cff1e251f0d
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-05-20 00:34:55 +0300

    Update fonts related stuff (#26)

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -10,6 +10,7 @@ ln -sf "${PWD}/.bashrc" "${HOME}/.bashrc"
 
 ./install/ubuntu/update.sh
 ./install/ubuntu/install-basic.sh
+./install/ubuntu/install-meslo-font.sh
 ./install/ubuntu/install-git.sh
 ./install/ubuntu/install-gh.sh
 ./install/ubuntu/install-tmux.sh
