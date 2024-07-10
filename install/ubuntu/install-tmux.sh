755fbdc9cbc31a6a920aa39226c5ffe86bf97435
Ivan Lemeshev <vanlmshv@gmail.com>
2024-07-10 17:20:58 +0300

    Update tmux configuration

--- install/ubuntu/install-tmux.sh
+++ install/ubuntu/install-tmux.sh
@@ -9,13 +9,5 @@ print_header "Installing: tmux"
 sudo apt install -y tmux
 
 print_header "Configuring: tmux"
-if prompt_yes_no "Do you want to use .tmux (https://github.com/gpakosz/.tmux.git) configuration?"; then
-    [[ -d ${PWD}/.tmux ]] || git clone https://github.com/gpakosz/.tmux.git
-
-    print_header "Linking configs: tmux (.tmux)"
-    ln -sf "${PWD}/.tmux/.tmux.conf" "${HOME}/.tmux.conf"
-    ln -sf "${PWD}/.tmux.conf.local" "${HOME}/.tmux.conf.local"
-else
-    print_header "Linking configs: tmux (personal)"
-    ln -sf "${PWD}/.tmux.conf" "${HOME}/.tmux.conf"
-fi
+[[ -d ${HOME}/.tmux/plugins/tpm ]] || git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+ln -sf "${PWD}/.tmux.conf" "${HOME}/.tmux.conf"
