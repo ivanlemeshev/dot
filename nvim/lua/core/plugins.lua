e89cf4896f45b53ebf32e07abd714721859a10dd
Ivan Lemeshev <vanlmshv@gmail.com>
2024-08-05 15:26:48 +0300

    Disable the checker notifications in lazy.nvim

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -30,5 +30,5 @@ require('lazy').setup({
         { import = 'plugins' },
     },
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = true, notify = false },
 })
