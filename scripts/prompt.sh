757d08e13b4c88e3943cbd6950b1d5b934605daf
Ivan Lemeshev <vanlmshv@gmail.com>
2024-08-11 20:14:56 +0300

    Fix ubuntu setup

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -12,9 +12,14 @@ function prompt_yes_no() {
 }
 
 function prompt_input() {
-    while [[ -z $input ]]; do
-        read -r -p "$1: " input
-    done
+    if [[ -n "$2" ]]; then
+        read -r -p "$1 ($2): " input
+    else
+        while [[ -z $input ]]; do
+            read -r -p "$1: " input
+        done
+    fi
+    [ -n "${input}" ] || input="$2"
 
     input="${input#"${input%%[![:space:]]*}"}" # Trim leading whitespace
     input="${input%"${input##*[![:space:]]}"}" # Trim trailing whitespace
