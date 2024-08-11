2e2b69b393f0c4987c7cdf4075a704926aa003eb
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-08-11 20:27:05 +0300

    Update scripts (#74)

--- install/macos/install-starship.sh
+++ install/macos/install-starship.sh
@@ -6,14 +6,13 @@ source ./scripts/print.sh
 source ./scripts/prompt.sh
 
 print_header "Congiguring: shell prompt"
-if prompt_yes_no "Do you want to use starship (https://starship.rs)?"; then
-    print_header "Installing: starship"
-    brew install starship
 
-    print_header "Checking version: starship"
-    starship -V
+print_header "Installing: starship"
+brew install starship
 
-    print_header "Linking configs: starship"
-    [[ -d "${HOME}/.config" ]] || mkdir -p "${HOME}/.config"
-    ln -sf "${PWD}/starship.toml" "${HOME}/.config/starship.toml"
-fi
+print_header "Checking version: starship"
+starship -V
+
+print_header "Linking configs: starship"
+[[ -d "${HOME}/.config" ]] || mkdir -p "${HOME}/.config"
+ln -sf "${PWD}/starship.toml" "${HOME}/.config/starship.toml"
