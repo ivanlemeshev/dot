89b7e6641fbd29d4259adc7860921ab03703c723
Ivan Lemeshev <vanlmshv@gmail.com>
2024-08-11 20:26:48 +0300

    Update scripts

--- install/macos/install-fish.sh
+++ install/macos/install-fish.sh
@@ -6,23 +6,22 @@ source scripts/print.sh
 source scripts/prompt.sh
 
 print_header "Congiguring: shell"
-if prompt_yes_no "Do you want to use the fish shell (https://fishshell.com)?"; then
-    print_header "Installing: fish"
-    brew install fish
-    sudo chsh -s "$(which fish)" "$(whoami)"
 
-    print_header "Checking version: fish"
-    fish -v
+print_header "Installing: fish"
+brew install fish
+sudo chsh -s "$(which fish)" "$(whoami)"
 
-    print_header "Linking configs: fish"
-    [[ -d "${HOME}/.config/fish" ]] || mkdir -p "${HOME}/.config/fish"
-    ln -sf "${PWD}/config.fish" "${HOME}/.config/fish/config.fish"
+print_header "Checking version: fish"
+fish -v
 
-    # https://github.com/jorgebucaran/fisher
-    print_header "Installing: fisher"
-    fish -C "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && exit"
+print_header "Linking configs: fish"
+[[ -d "${HOME}/.config/fish" ]] || mkdir -p "${HOME}/.config/fish"
+ln -sf "${PWD}/config.fish" "${HOME}/.config/fish/config.fish"
 
-    # https://github.com/PatrickF1/fzf.fish
-    print_header "Installing: PatrickF1/fzf.fish"
-    fish -C "fisher install PatrickF1/fzf.fish && exit"
-fi
+# https://github.com/jorgebucaran/fisher
+print_header "Installing: fisher"
+fish -C "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && exit"
+
+# https://github.com/PatrickF1/fzf.fish
+print_header "Installing: PatrickF1/fzf.fish"
+fish -C "fisher install PatrickF1/fzf.fish && exit"
--- install/ubuntu/install-starship.sh
+++ install/ubuntu/install-starship.sh
@@ -6,14 +6,14 @@ source ./scripts/print.sh
 source ./scripts/prompt.sh
 
 print_header "Congiguring: shell prompt"
-if prompt_yes_no "Do you want to use starship (https://starship.rs)?"; then
-    print_header "Installing: starship"
-    curl -sS https://starship.rs/install.sh | sh
 
-    print_header "Checking version: starship"
-    starship -V
+print_header "Installing: starship"
+curl -sS https://starship.rs/install.sh | sh
 
-    print_header "Linking configs: starship"
-    [[ -d "${HOME}/.config" ]] || mkdir -p "${HOME}/.config"
-    ln -sf "${PWD}/starship.toml" "${HOME}/.config/starship.toml"
-fi
+print_header "Checking version: starship"
+starship -V
+
+print_header "Linking configs: starship"
+[[ -d "${HOME}/.config" ]] || mkdir -p "${HOME}/.config"
+ln -sf "${PWD}/starship.toml" "${HOME}/.config/starship.toml"
+
