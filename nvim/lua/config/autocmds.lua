2338535456b7a091aec7c748e89d4415d3d3cfa9
Ivan Lemeshev <vanlmshv@gmail.com>
2024-08-19 22:18:44 +0300

    Update nvim configuration

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,17 +3,17 @@
 -- Add any additional autocmds here
 
 -- Automatically save changes to a file if the current buffer is modified.
-vim.api.nvim_create_autocmd({ "TextChanged", "BufLeave", "InsertLeave" }, {
+vim.api.nvim_create_autocmd({ "BufLeave", "InsertLeave" }, {
   callback = function()
     if vim.bo.modified then
       vim.api.nvim_exec_autocmds("BufWritePre", {
-        pattern = vim.fn.expand("%")
+        pattern = vim.fn.expand("%"),
       })
 
       vim.api.nvim_command("write")
 
       vim.api.nvim_exec_autocmds("BufWritePost", {
-        pattern = vim.fn.expand("%")
+        pattern = vim.fn.expand("%"),
       })
     end
   end,
