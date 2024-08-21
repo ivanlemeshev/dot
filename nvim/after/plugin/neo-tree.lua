5c1b1fa846435774dfa4efafcd4c095655db29d1
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-08-21 18:49:32 +0300

    Update nvim configuration (#85)

---
+++ nvim/after/plugin/neo-tree.lua
@@ -0,0 +1,11 @@
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_by_name = {
+        ".git",
+      },
+    },
+  },
+})
