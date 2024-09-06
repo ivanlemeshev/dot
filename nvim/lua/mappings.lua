75a639d8f1486fcd2cce6b44fc4a30d5f6e84b75
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-09-06 15:16:38 +0300

    Update key mappings for nvim (#113)

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,7 +7,11 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
 map("n", "<leader>ct", vim.cmd.CopilotChat, { desc = "Open the Copilot chat" })
 map("n", "<leader>ce", vim.cmd.CopilotChatExplain, { desc = "Ask Copilot to explain the selected code" })
+map("i", "<C-f>", 'copilot#Accept("\\<CR>")', {
+  desc = "Complete the current line using Copilot",
+  expr = true,
+  replace_keycodes = false,
+})
+vim.g.copilot_no_esc_map = true
