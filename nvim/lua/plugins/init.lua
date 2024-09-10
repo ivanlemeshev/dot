da0bdc67cfb2676d6e7dbf2a54b9c7dc1902eea6
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-09-10 20:54:54 +0300

    Update nvim configuration (#118)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -75,4 +75,21 @@ return {
       debug = true,
     },
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      filters = {
+        enable = true,
+        git_ignored = false,
+        dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        no_bookmark = false,
+        custom = {
+          "^.git$",
+        },
+        exclude = {},
+      },
+    },
+  },
 }
