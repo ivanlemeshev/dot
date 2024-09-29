be4fd853484417167e55db553a1ee5741a0a8b0a
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-29 20:53:25 +0300

    Update

--- install/ubuntu/install-nerd-font.sh
+++ install/ubuntu/install-nerd-font.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-source ./scripts/print.sh
+source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 
-print_header "Installing: JetBrainsMono Nerd Font"
+print_header "Installing JetBrainsMono Nerd Font"
+
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
 sudo tar -xf ./JetBrainsMono.tar.xz -C /usr/local/share/fonts
 sudo fc-cache -fv
--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -8,13 +8,13 @@ set -e
 ln -sf "${PWD}/.wezterm.lua" "${HOME}/.wezterm.lua"
 
 ./install/ubuntu/update.sh
-# ./install/ubuntu/install-basic.sh
+./install/ubuntu/install-basic.sh
 ./install/ubuntu/install-git.sh
+./install/ubuntu/install-nerd-font.sh
 ./install/ubuntu/install-tmux.sh
 # ./install/ubuntu/install-bat.sh
 # ./install/ubuntu/install-bottom.sh
 # ./install/ubuntu/install-vivid.sh
-# ./install/ubuntu/install-nerd-font.sh
 # ./install/ubuntu/install-gh.sh
 # ./install/ubuntu/install-starship.sh
 # ./install/ubuntu/install-mise.sh
