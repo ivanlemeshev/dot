9144f87770e05ec828c342af077d3d40b38d8bc4
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-29 20:32:29 +0300

    Update

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,8 +6,8 @@ set -e
 # The -s option tells ln to create a symbolic link.
 # The -f option tells it to replace the file at the target location if it
 # already exists.
-# ln -sf "${PWD}/.bashrc" "${HOME}/.bashrc"
-# ln -sf "${PWD}/.wezterm.lua" "${HOME}/.wezterm.lua"
+ln -sf "${PWD}/.bashrc" "${HOME}/.bashrc"
+ln -sf "${PWD}/.wezterm.lua" "${HOME}/.wezterm.lua"
 
 # ./install/ubuntu/update.sh
 # ./install/ubuntu/install-basic.sh
