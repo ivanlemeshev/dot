d26770f19278724a17b1485448714171fec2fd42
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-29 20:16:36 +0300

    Add tmux

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -12,12 +12,12 @@ set -e
 # ./install/ubuntu/update.sh
 # ./install/ubuntu/install-basic.sh
 ./install/ubuntu/install-git.sh
+./install/ubuntu/install-tmux.sh
 # ./install/ubuntu/install-bat.sh
 # ./install/ubuntu/install-bottom.sh
 # ./install/ubuntu/install-vivid.sh
 # ./install/ubuntu/install-nerd-font.sh
 # ./install/ubuntu/install-gh.sh
-# ./install/ubuntu/install-tmux.sh
 # ./install/ubuntu/install-starship.sh
 # ./install/ubuntu/install-mise.sh
 # ./install/ubuntu/install-fish.sh
