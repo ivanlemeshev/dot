31c26efbfe2d2788c1d023e3e780ed0661ad2503
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-29 21:08:15 +0300

    Update

--- install/ubuntu/install-basic.sh
+++ install/ubuntu/install-basic.sh
@@ -8,11 +8,12 @@ print_header "Installing basic packages"
 
 packages=(
 	# Essential tools
-	"curl"     # A tool for transferring data from or to a server using URLs.
-	"wget"     # A tool for downloading files from the Internet.
-	"vim"      # A programmer's text editor.
-	"htop"     # An interactive process viewer.
-	"xz-utils" # A set of tools for working with xz compressed files.
+	"curl"       # A tool for transferring data from or to a server using URLs.
+	"wget"       # A tool for downloading files from the Internet.
+	"vim"        # A programmer's text editor.
+	"htop"       # An interactive process viewer.
+	"xz-utils"   # A set of tools for working with xz compressed files.
+	"fontconfig" # A library for configuring and customizing font access.
 )
 
 sudo apt-get install -y ${packages[*]}
