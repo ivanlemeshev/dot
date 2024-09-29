dc27eacfa1375259025031c3c315f70bb3052328
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-29 20:45:22 +0300

    Fix typo

--- install/ubuntu/update.sh
+++ install/ubuntu/update.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 
-print_header "Updatiing the system"
+print_header "Updating the system"
 sudo apt-get update
 sudo apt-get upgrade -y
