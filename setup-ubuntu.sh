c80372508d40f61d2c53e47a09c1737d63d6f759
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-29 20:40:29 +0300

    Remove bashrc

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-# The ln ccommand makes links between files.
 # The -s option tells ln to create a symbolic link.
-# The -f option tells it to replace the file at the target location if it
+# The -f option tells ln to replace the file at the target location if it
 # already exists.
-ln -sf "${PWD}/.bashrc" "${HOME}/.bashrc"
 ln -sf "${PWD}/.wezterm.lua" "${HOME}/.wezterm.lua"
 
 # ./install/ubuntu/update.sh
