ce8d57365d0778c5148265a9c8697b604bbf8bce
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-30 07:52:15 +0300

    Update

--- install/ubuntu/install-starship.sh
+++ install/ubuntu/install-starship.sh
@@ -13,25 +13,25 @@ starship_archive=starship.tar.gz
 starship_checksum=starship.tar.gz.sha256
 starship_checksum_temp=starship.sha256
 
-print_header "Installing the starship prompt"
+print_header "Starship: installing version ${starship_version}"
 
-print_header "Downloading archive"
+print_header "Starship: downloading archive"
 wget "${starship_url}" -O "${starship_archive}"
 
-print_header "Downloading checksum"
+print_header "Starship: downloading checksum"
 wget "${startship_checksum_url}" -O "${starship_checksum}"
 
-print_header "Verifying checksum"
+print_header "Starship: verifying checksum"
 echo "$(cat "${starship_checksum}")  ${starship_archive}" >"${starship_checksum_temp}"
 if ! sha256sum -c "${starship_checksum_temp}"; then
 	echo "Checksum verification failed! The archive may be corrupted."
 	exit 1
 fi
 
-print_header "Installing to ${starship_installation_path}"
+print_header "Starship: installing to ${starship_installation_path}"
 sudo tar -C "${starship_installation_path}" -xzf "${starship_archive}"
 rm "${starship_archive}" "${starship_checksum}" "${starship_checksum_temp}"
 
-print_header "Creating a symbolic link for the starship configuration file"
+print_header "Starship: creating a symbolic link for the starship configuration file"
 [[ -d "${HOME}/.config" ]] || mkdir -p "${HOME}/.config"
 ln -sf "${PWD}/starship.toml" "${HOME}/.config/starship.toml"
