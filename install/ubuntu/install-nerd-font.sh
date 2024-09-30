b2d1e0e55f2eefdc6d9c36541f11ad4e8918a568
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-09-30 21:21:52 +0300

    Fixes (#152)

--- install/ubuntu/install-nerd-font.sh
+++ install/ubuntu/install-nerd-font.sh
@@ -9,3 +9,4 @@ print_header "Fonts: installing JetBrainsMono Nerd Font"
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
 sudo tar -xf ./JetBrainsMono.tar.xz -C /usr/local/share/fonts
 sudo fc-cache -fv
+rm JetBrainsMono.tar.xz
