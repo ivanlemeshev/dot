302fabda180d480b2c184790a1dc1fa2297b0c33
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-30 19:18:46 +0300

    Update

--- install/ubuntu/install-rust.sh
+++ install/ubuntu/install-rust.sh
@@ -6,6 +6,7 @@ source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 
 print_header "Rust: installing"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
-bash -l
+exec bash -l
 rustup default stable
 rustup completions fish >~/.config/fish/completions/rustup.fish
+exit
