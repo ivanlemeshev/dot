cdb553a97bdcc1a36386cc54fe00cd9ae34cb1fe
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-30 18:33:52 +0300

    Update

--- install/ubuntu/install-basic.sh
+++ install/ubuntu/install-basic.sh
@@ -11,6 +11,7 @@ packages=(
 	"wget"                       # A tool for downloading files from the Internet.
 	"vim"                        # A programmer's text editor.
 	"htop"                       # An interactive process viewer.
+	"unzip"                      # A utility for extracting, testing and viewing the contents of ZIP archives.
 	"xz-utils"                   # A set of tools for working with xz compressed files.
 	"fontconfig"                 # A library for configuring and customizing font access.
 	"fzf"                        # A command-line fuzzy finder.
