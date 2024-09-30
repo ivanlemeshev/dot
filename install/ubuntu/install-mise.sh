8d8abc7350ed7bef6b6167a671e961886aca6fa7
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-30 17:38:04 +0300

    Update

--- install/ubuntu/install-mise.sh
+++ install/ubuntu/install-mise.sh
@@ -8,7 +8,6 @@ mise_installation_path=/usr/local/bin
 
 # https://github.com/jdx/mise
 print_header "Mise: installing latest version"
-[[ ! -d "${mise_installation_path}" ]] && sudo mkdir -p "${mise_installation_path}"
 wget "https://mise.jdx.dev/mise-latest-linux-x64" -O "./mise"
 sudo mv "./mise" "${mise_installation_path}/mise"
 sudo chmod +x "${mise_installation_path}/mise"
--- install/ubuntu/install-vivid.sh
+++ install/ubuntu/install-vivid.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-source ./scripts/print.sh
+source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 
-VIVID_VERSION=0.10.1
+vivid_version=0.10.1
 
-print_header "Installing: vivid"
+print_header "Vivid: installing version ${vivid_version}"
 wget "https://github.com/sharkdp/vivid/releases/download/v${VIVID_VERSION}/vivid_${VIVID_VERSION}_amd64.deb"
 sudo dpkg -i "vivid_${VIVID_VERSION}_amd64.deb"
 rm "vivid_${VIVID_VERSION}_amd64.deb"
