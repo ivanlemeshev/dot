9162d7aa8a2c6b8c4da943f8ccabff1a4a30f56a
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-30 19:09:27 +0300

    Update

--- install/ubuntu/install-fish.sh
+++ install/ubuntu/install-fish.sh
@@ -23,19 +23,9 @@ fzf_fish_version="10.3"
 fzf_fish_url="https://github.com/PatrickF1/fzf.fish/archive/refs/tags/v${fzf_fish_version}.tar.gz"
 fzf_fish_archive="fzf.fish.tar.gz"
 
-print_header "Fish: downloading fzf.fish"
-wget "${fzf_fish_url}" -O "${fzf_fish_archive}"
+print_header "Fish: installing plugin manager Fisher"
+fish -C "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && exit"
 
-print_header "Fish: extracting fzf.fish"
-tar -xzf "${fzf_fish_archive}"
-
-print_header "Fish: installing fzf.fish"
-[[ -d "${HOME}/.config/fish/completions" ]] || mkdir -p "${HOME}/.config/fish/completions"
-[[ -d "${HOME}/.config/fish/conf.d" ]] || mkdir -p "${HOME}/.config/fish/conf.d"
-[[ -d "${HOME}/.config/fish/functions" ]] || mkdir -p "${HOME}/.config/fish/functions"
-cp -a "${PWD}/fzf.fish-${fzf_fish_version}/completions/" "${HOME}/.config/fish/completions/"
-cp -a "${PWD}/fzf.fish-${fzf_fish_version}/conf.d/" "${HOME}/.config/fish/conf.d/"
-cp -a "${PWD}/fzf.fish-${fzf_fish_version}/functions/" "${HOME}/.config/fish/functions/"
-
-print_header "Fish: cleaning up"
-rm -rf "${fzf_fish_archive}" "fzf.fish-${fzf_fish_version}"
+# https://github.com/PatrickF1/fzf.fish
+print_header "Fish: fzf.fish"
+fish -C "fisher install PatrickF1/fzf.fish && exit"
