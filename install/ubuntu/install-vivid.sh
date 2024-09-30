a3554fe1b77a4b833aa81fd7e96c4524911d7ffa
Ivan Lemeshev <vanlmshv@gmail.com>
2024-09-30 17:39:00 +0300

    Update

--- install/ubuntu/install-vivid.sh
+++ install/ubuntu/install-vivid.sh
@@ -7,6 +7,6 @@ source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 vivid_version=0.10.1
 
 print_header "Vivid: installing version ${vivid_version}"
-wget "https://github.com/sharkdp/vivid/releases/download/v${VIVID_VERSION}/vivid_${VIVID_VERSION}_amd64.deb"
-sudo dpkg -i "vivid_${VIVID_VERSION}_amd64.deb"
-rm "vivid_${VIVID_VERSION}_amd64.deb"
+wget "https://github.com/sharkdp/vivid/releases/download/v${vivid_version}/vivid_${vivid_version}_amd64.deb"
+sudo dpkg -i "vivid_${vivid_version}_amd64.deb"
+rm "vivid_${vivid_version}_amd64.deb"
