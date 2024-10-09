f4b24c5b9f53cd2c401395afa96c055c596970f1
Ivan Lemeshev <vanlmshv@gmail.com>
2024-10-09 20:31:42 +0300

    Improve autocompletion in Neovim

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,16 +1,19 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  event = "VimEnter",
   dependencies = {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lua",
+    "zbirenbaum/copilot-cmp",
   },
 
   config = function()
     local cmp = require("cmp")
+
+    require("copilot_cmp").setup()
 
     local kind_icons = {
       Text = "󰉿",
@@ -38,44 +41,67 @@ return {
       Event = "",
       Operator = "󰆕",
       TypeParameter = "󰊄",
+      Copilot = "",
     }
+
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+      end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+    end
 
     cmp.setup({
       performance = {
         max_view_entries = 10,
       },
-      preselect = cmp.PreselectMode.Item,
+      preselect = cmp.PreselectMode.None,
       completion = {
         keyword_length = 1,
-        completeopt = "menu,menuone,popup,noinsert,fuzzy",
+        completeopt = "menu,menuone,popup,noselect,fuzzy",
       },
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          -- Configuration to use with copilot.
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
+        ["<CR>"] = cmp.mapping({
+          -- If nothing is selected (including preselections) add a newline as usual.
+          -- If something has explicitly been selected by the user, select it.
+          i = function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            else
+              fallback()
+            end
+          end,
+          s = cmp.mapping.confirm({ select = true }),
+          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
       }),
       sources = {
-        { name = "buffer" },
-        { name = "path" },
-        { name = "nvim_lua" },
-        { name = "nvim_lsp" },
+        { name = "copilot",  group_index = 2, priority = 10 },
+        { name = "nvim_lsp", group_index = 2, priority = 9 },
+        { name = "nvim_lua", group_index = 2, priority = 8 },
+        { name = "buffer",   group_index = 2, priority = 7 },
+        { name = "path",     group_index = 2, priority = 6 },
       },
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       },
       formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          vim_item.kind = kind_icons[vim_item.kind]
-          vim_item.menu = ({
-            buffer = "[Buffer]",
-            path = "[Path]",
-            cmdline = "[Cmdline]",
-            nvim_lua = "[Lua]",
-            nvim_lsp = "[LSP]",
-          })[entry.source.name]
+        fields = { "kind", "abbr" },
+        format = function(_, vim_item)
+          vim_item.kind = kind_icons[vim_item.kind] or ""
           return vim_item
         end,
       },
@@ -89,14 +115,10 @@ return {
       },
     })
 
-    -- Completions for command mode.
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
-      }),
+    cmp.setup.filetype({ "markdown" }, {
+      window = {
+        documentation = cmp.config.disable,
+      },
     })
   end,
 }
