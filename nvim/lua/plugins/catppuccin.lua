d0d764ba0eee91c94910bffcd5ee4852694033cd
Ivan Lemeshev <vanlmshv@gmail.com>
2024-10-09 18:36:30 +0300

    Minor changes

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -6,10 +6,16 @@ return {
       integrations = {
         -- For more plugins integrations please scroll down
         -- https://github.com/catppuccin/nvim#integrations
+        barbar = true,
         cmp = true,
+        fidget = true,
         gitsigns = true,
+        markdown = true,
+        mason = true,
+        neottest = true,
         neotree = true,
         treesitter = true,
+        treesitter_context = true,
       },
     })
   end,
