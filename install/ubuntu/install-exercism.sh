466a56fbb37c808566d61373595dd99355bd8ef2
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-10-21 01:53:58 +0300

    Add install scripts (#161)

---
+++ install/ubuntu/install-exercism.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+source "$(dirname "$0")/../../scripts/functions/print_header.sh"
+
+exercism_version=3.5.2
+
+print_header "Exercism: installing version ${exercism_version}"
+
+wget "https://github.com/exercism/cli/releases/download/v${exercism_version}/exercism-${exercism_version}-linux-x86_64.tar.gz"
+mkdir ./exercism
+tar -xf "exercism-${exercism_version}-linux-x86_64.tar.gz" -C ./exercism
+sudo mv ./exercism/exercism /usr/local/bin
+mv ./exercism/shell/exercism.fish $HOME/.config/fish/completions
+rm "exercism-${exercism_version}-linux-x86_64.tar.gz"
+rm -rf ./exercism
