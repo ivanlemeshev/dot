b927e877cb01ff247e6313591f33c89b7491bf56
Ivan Lemeshev <vanlmshv@gmail.com>
2024-10-26 18:57:05 +0300

    Configure tabs for different filetypes in Neovim

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,13 +1,5 @@
 return {
   {
-    -- Automatically adjust 'shiftwidth' and 'expandtab' heuristically based
-    -- on the current file, or, in the case the current file is new, blank, or
-    -- otherwise insufficient, by looking at other files of the same type in
-    -- the current and parent directories.
-    "tpope/vim-sleuth",
-    event = "BufRead",
-  },
-  {
     -- Git integration for Vim
     "tpope/vim-fugitive",
     event = "BufRead",
