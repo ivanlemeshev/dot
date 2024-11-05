baa29b3fd5787995ca23ca177b8c111d7c31f810
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-11-05 16:50:22 +0200

    Update Python version (#184)

--- install/ubuntu/install-python.sh
+++ install/ubuntu/install-python.sh
@@ -4,7 +4,7 @@ set -e
 
 source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 
-python_version="3.12.4"
+python_version="3.13.0"
 
 print_header "Python: installing version ${python_version}"
 fish -C "mise use -g python@${python_version} && exit"
