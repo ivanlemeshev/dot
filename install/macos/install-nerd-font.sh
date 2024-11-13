ffe81f53c3ce9fcb9f90adbf9ade9cb279a2cc95
Ivan Lemeshev <vanlmshv@gmail.com>
2024-11-13 23:02:29 +0200

    Revert "Update fonts (#194)"
    This reverts commit 5fc2c06cf171aadd6c4f65bf59f6dbb0f7985237.

--- install/macos/install-nerd-font.sh
+++ install/macos/install-nerd-font.sh
@@ -4,6 +4,6 @@ set -e
 
 source ./scripts/print.sh
 
-print_header "Installing: SauceCodePro Nerd Font"
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/SauceCodePro.tar.xz
-sudo tar -xf ./SauceCodePro.tar.xz -C /Library/Fonts
+print_header "Installing: RobotoMono Nerd Font"
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/RobotoMono.tar.xz
+sudo tar -xf ./RobotoMono.tar.xz -C /Library/Fonts
--- install/windows/install-nerd-font.ps1
+++ install/windows/install-nerd-font.ps1
@@ -1,8 +1,8 @@
-Write-Host "Installing SourceCodePro Nerd fonts..."
+Write-Host "Installing RobotoMono Nerd fonts..."
 
 Write-Host "Downloading the fonts archive..."
 
-$fontFilename = "SourceCodePro.zip"
+$fontFilename = "RobotoMono.zip"
 $fontUrl = "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$fontFilename"
 $fontOutputFile = "$scriptDirectory\$fontFilename"
 
