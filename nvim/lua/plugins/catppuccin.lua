eaff26f5db2d7f6a03cdbee9bf68533dd648b7b4
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-11-24 00:43:37 +0200

    Update nvim configuration (#203)

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,22 +1,30 @@
 return {
   "catppuccin/nvim",
+  lazy = false,
   priority = 1000,
-  setup = function()
+  config = function()
     require("catppuccin").setup({
       integrations = {
         -- For more plugins integrations please scroll down
         -- https://github.com/catppuccin/nvim#integrations
-        barbar = true,
         cmp = true,
         fidget = true,
         gitsigns = true,
         markdown = true,
         mason = true,
-        neottest = true,
-        neotree = true,
+        neotest = true,
+        noice = true,
+        notify = true,
+        nvimtree = true,
+        render_markdown = true,
         treesitter = true,
         treesitter_context = true,
+        lsp_trouble = true,
+        which_key = true,
       },
     })
+
+    -- Setup must be called before loading.
+    vim.cmd.colorscheme("catppuccin-mocha")
   end,
 }
--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -6,30 +6,4 @@ return {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
-  {
-    "rcarriga/nvim-notify",
-    config = function()
-      require("notify").setup({
-        stages = "static",
-        timeout = 5000,
-        vim.cmd([[
-          highlight NotifyERRORBorder guifg=#F38BA8
-          highlight NotifyWARNBorder guifg=#FAB387
-          highlight NotifyINFOBorder guifg=#A6E3A1
-          highlight NotifyDEBUGBorder guifg=#9399B2
-          highlight NotifyTRACEBorder guifg=#CBA6F7
-          highlight NotifyERRORIcon guifg=#F38BA8
-          highlight NotifyWARNIcon guifg=#FAB387
-          highlight NotifyINFOIcon guifg=#A6E3A1
-          highlight NotifyDEBUGIcon guifg=#9399B2
-          highlight NotifyTRACEIcon guifg=#CBA6F7
-          highlight NotifyERRORTitle  guifg=#F38BA8
-          highlight NotifyWARNTitle guifg=#FAB387
-          highlight NotifyINFOTitle guifg=#A6E3A1
-          highlight NotifyDEBUGTitle  guifg=#9399B2
-          highlight NotifyTRACETitle  guifg=#CBA6F7
-        ]]),
-      })
-    end,
-  },
 }
