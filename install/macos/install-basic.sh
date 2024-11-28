8f01ea96ccddfd35affa108486c42c54df93445e
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-11-28 09:21:06 +0200

    Add homebrew (#213)

--- install/macos/install-basic.sh
+++ install/macos/install-basic.sh
@@ -4,19 +4,22 @@ set -e
 
 source ./scripts/print.sh
 
+print_header "Installing: Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 print_header "Installing: basic packages"
 brew install \
-    openssl \
-    readline \
-    sqlite3 \
-    xz \
-    zlib \
-    tcl-tk \
-    vivid \
-    tree \
-    fd \
-    fzf \
-    bottom \
-    go \
-    node \
-    lazygit \
+	openssl \
+	readline \
+	sqlite3 \
+	xz \
+	zlib \
+	tcl-tk \
+	vivid \
+	tree \
+	fd \
+	fzf \
+	bottom \
+	go \
+	node \
+	lazygit
