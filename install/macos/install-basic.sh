c6f0391e09b06481f0dfccbef7b585f7c57db04a
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-12-03 10:15:10 +0200

    Update macos basic packages (#225)

--- install/macos/install-basic.sh
+++ install/macos/install-basic.sh
@@ -5,19 +5,24 @@ set -e
 source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 
 print_header "Installing: basic packages"
-brew install \
-	openssl \
-	readline \
-	sqlite3 \
-	xz \
-	zlib \
-	tcl-tk \
-	vivid \
-	tree \
-	fd \
-	fzf \
-	bottom \
-	go \
-	node \
-	lazygit \
-	wget
+
+packages=(
+	"openssl"
+	"readline"
+	"sqlite3"
+	"xz"
+	"zlib"
+	"tcl-tk"
+	"vivid"
+	"tree"
+	"fd"
+	"fzf"
+	"bottom"
+	"go"
+	"node"
+	"lazygit"
+	"wget"
+	"ripgrep"
+)
+
+brew install ${packages[*]}
