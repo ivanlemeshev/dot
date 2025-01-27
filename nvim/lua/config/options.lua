fd7abb1e7ac0b7184fe0548c8a31ee3a9368f9be
Ivan Lemeshev <vanlmshv@gmail.com>
2025-01-27 10:09:35 +0200

    Add terraform

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -261,3 +261,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 2
   end,
 })
+
+-- Terraform settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "terraform",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
