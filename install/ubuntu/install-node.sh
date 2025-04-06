331f359e1963b4d25acd76ff9627e45f0f9535e2
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2025-04-07 00:38:20 +0300

    Update nodejs version (#242)

--- install/ubuntu/install-node.sh
+++ install/ubuntu/install-node.sh
@@ -4,7 +4,7 @@ set -e
 
 source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 
-node_version="20.16.0"
+node_version="22.14.0"
 
 print_header "Node.js: installing version ${node_version}"
 fish -C "mise use -g node@${node_version} && exit"
