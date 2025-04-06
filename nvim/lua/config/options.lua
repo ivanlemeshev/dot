bf45baa17ce2424b692af8fd92ac5b13665e41b6
Ivan Lemeshev <vanlmshv@gmail.com>
2025-04-07 01:10:14 +0300

    Add indentaion settings for TypeScript

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -283,3 +283,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 4
   end,
 })
+
+-- TypeScript settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "typescript",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
