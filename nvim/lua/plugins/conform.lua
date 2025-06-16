8de1349a69552c34b59fa8e6f222193d224162d4
Ivan Lemeshev <ivan.lemeshev@golfgamebook.com>
2025-06-16 13:44:04 +0300

    Migrate nvim to conform and lint

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,18 +4,38 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
+        c = { "clang_format" },
+        go = {
+          "gofumpt",
+          "goimports",
+          -- "golangci-lint", -- TODO
+          -- "golines",
+        },
+        javascript = { "biome" },
         json = { "biome" },
         lua = { "stylua" },
         markdown = { "prettier" },
-        python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-        go = { "gofumpt", "goimports" },
-        javascript = { "biome" },
+        proto = { "buf" },
+        python = {
+          "ruff_fix",
+          "ruff_format",
+          "ruff_organize_imports",
+        },
+        sh = { "shfmt" },
       },
       format_on_save = {
-        -- These options will be passed to conform.format()
         timeout_ms = 500,
         lsp_format = "fallback",
       },
     })
   end,
 }
+
+-- TODO: configure shfmt to use Google's Style guide
+--   formatting.shfmt.with({
+--     filetypes = { "sh" },
+--     -- To get the formatting appropriate for Google's Style guide,
+--     -- use shfmt -i 2 -ci.
+--     -- https://google.github.io/styleguide/shellguide.html
+--     args = { "-filename", "$FILENAME", "-i", "2", "-ci" },
+--   }),
