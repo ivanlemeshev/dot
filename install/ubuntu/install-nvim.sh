6a069c4bea9dade6d1262e6eb919dddbf72829c5
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2025-07-07 13:11:37 +0300

    Update nvim installation for Ububntu (#259)

--- install/ubuntu/install-nvim.sh
+++ install/ubuntu/install-nvim.sh
@@ -5,8 +5,9 @@ set -e
 source "$(dirname "$0")/../../scripts/functions/print_header.sh"
 
 print_header "Neovim: installing"
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+# http://github.com/neovim/neovim/blob/master/INSTALL.md#linux
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 [[ -d /opt/nvim ]] && sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 ln -sf "${PWD}/nvim" "${HOME}/.config/nvim"
