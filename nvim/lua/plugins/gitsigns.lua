8afc6d6b61cf0ddfe11beb2b795d5fa6f9b95f7d
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2025-07-15 11:11:01 +0300

    Add mappings for gitsigns (#261)

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -16,4 +16,14 @@ return {
       changedelete = { text = "~" },
     },
   },
+  config = function(_, opts)
+    require("gitsigns").setup(opts)
+
+    local map = vim.keymap.set
+
+    map("n", "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "Git: blame line" })
+    map("n", "<leader>gd", "<cmd>Gitsigns diffthis<CR>", { desc = "Git: diff this" })
+    map("n", "<leader>gn", "<cmd>Gitsigns next_hunk<CR>", { desc = "Git: next hunk" })
+    map("n", "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Git: previous hunk" })
+  end,
 }
