5bb578a61268c784ce4915545df1f5137ac24c7f
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2025-08-26 12:20:44 +0300

    Add markdown indentation settings (#281)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -311,3 +311,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 2
   end,
 })
+
+-- Markdown settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
