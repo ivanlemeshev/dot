ea520179bfbc09ff6832db05491efac0c9e4ecfa
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2025-09-04 22:06:42 +0300

    Configure rio terminal in Windows (#290)

--- install/windows/install-choco-packages.ps1
+++ install/windows/install-choco-packages.ps1
@@ -2,4 +2,5 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; `
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; `
     iex ((New-Object System.Net.WebClient).DownloadString("https://community.chocolatey.org/install.ps1"))
 
+choco upgrade all -y
 choco install -y powershell wezterm starship gh make zig rio-terminal
