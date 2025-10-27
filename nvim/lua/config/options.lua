eee7a8c95af2a78e85dffa844864fba15f08c4ca
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2025-10-27 13:11:45 +0200

    Update nvim configuration (#305)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -298,9 +298,9 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "proto",
   callback = function()
     vim.opt_local.expandtab = true
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.softtabstop = 4
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
   end,
 })
 
