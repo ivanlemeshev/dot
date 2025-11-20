0fec7d80da3cc729fb1a74f445ee56985a12fe4b
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2025-11-20 21:08:22 +0200

    Add terraform support (#311)

---
+++ install/ubuntu/install-terraform.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+source "$(dirname "$0")/../../scripts/functions/print_header.sh"
+
+print_header "Terraform: installing"
+
+hashicorp_repo="deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+target_file="/etc/apt/sources.list.d/hashicorp.list"
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+grep -qF "$hashicorp_repo" "$target_file" 2>/dev/null || echo "$hashicorp_repo" | sudo tee "$target_file" >/dev/null
+sudo apt update && sudo apt install terraform
