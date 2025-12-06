b9ce0db99a0a57405bc2128f17a42e4f85cc3c52
Ivan Lemeshev <vanlmshv@gmail.com>
2025-12-06 18:08:03 +0200

    Add Ubuntu Desktop setup

---
+++ setup-ubuntu-desktop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# The -s option tells ln to create a symbolic link.
+# The -f option tells ln to replace the file at the target location if it
+# already exists.
+ln -sf "${PWD}/.vimrc" "${HOME}/.vimrc"
+
+./install/ubuntu/update.sh
+./install/ubuntu/install-basic.sh
+./install/ubuntu/install-git.sh
+./install/ubuntu/install-asdf.sh
+./install/ubuntu/install-bat.sh
+./install/ubuntu/install-vivid.sh
+./install/ubuntu/install-nerd-font.sh
+./install/ubuntu/install-tmux.sh
+./install/ubuntu/install-starship.sh
+./install/ubuntu/install-fish.sh
+./install/ubuntu/install-bottom.sh
+./install/ubuntu/install-go.sh
+./install/ubuntu/install-zig.sh
+./install/ubuntu/install-python.sh
+./install/ubuntu/install-node.sh
+./install/ubuntu/install-lua.sh
+./install/ubuntu/install-rust.sh
+./install/ubuntu/install-gh.sh
+./install/ubuntu/install-nvim.sh
+./install/ubuntu/install-yt-dlp.sh
+./install/ubuntu/clean.sh
+
+exec fish -l
