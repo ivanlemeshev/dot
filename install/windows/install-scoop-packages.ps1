661aefb371ad16f8dc4206dc540b8cf5aed64afd
Ivan Lemeshev <vanlmshv@gmail.com>
2026-01-03 23:49:02 +0200

    Remove rio terminal

--- install/windows/install-scoop-packages.ps1
+++
@@ -1,8 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expressionchoco
-
-scoop update --all
-
-scoop bucket add extras
-
-scoop install extras/rio
