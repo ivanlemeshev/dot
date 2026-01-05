fc1a463773babae7a7bbbb20d2ac930077bce682
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2026-01-05 08:53:51 +0200

    Add CSV view to nvim (#321)

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -63,4 +63,46 @@ return {
       },
     },
   },
+  {
+    "hat0uma/csvview.nvim",
+    ---@module "csvview"
+    ---@type CsvView.Options
+    opts = {
+      parser = { comments = { "#", "//" } },
+      view = {
+        display_mode = "border",
+      },
+      keymaps = {
+        -- Text objects for selecting fields
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        -- Excel-like navigation:
+        -- Use <Tab> and <S-Tab> to move horizontally between fields.
+        -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+        -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      },
+    },
+    config = function(_, opts)
+      require("csvview").setup(opts)
+      -- Disable in insert mode, re-enable when leaving insert mode
+      local group = vim.api.nvim_create_augroup("CsvViewInsertMode", { clear = true })
+      vim.api.nvim_create_autocmd("InsertEnter", {
+        group = group,
+        callback = function()
+          vim.cmd("CsvViewDisable")
+        end,
+      })
+      vim.api.nvim_create_autocmd("InsertLeave", {
+        group = group,
+        callback = function()
+          vim.cmd("CsvViewEnable")
+        end,
+      })
+    end,
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+  },
 }
