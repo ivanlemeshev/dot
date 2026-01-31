bba2657f5e1ee7fd8d5a5507191676cccf8ea7c5
Ivan Lemeshev <vanlmshv@gmail.com>
2026-01-31 21:15:10 +0200

    Refactoring

--- v2/lib/ui.sh
+++ v2/lib/ui.sh
@@ -9,7 +9,7 @@ YELLOW='\e[33m'
 RESET='\e[0m' #resets the color to default
 
 # Print a header with simple ASCII box
-function print_header() {
+function ui.print_header() {
   local title="$*"
   local width=80
   local padding=$((width - 2 - ${#title}))
@@ -37,21 +37,21 @@ function print_header() {
 }
 
 # Print an info message
-function print_info() {
+function ui.print_info() {
   echo -e "${BLUE}[INFO]${RESET} $*"
 }
 
 # Print a success message
-function print_success() {
+function ui.print_success() {
   echo -e "${GREEN}[ OK ]${RESET} $*"
 }
 
 # Print an error message
-function error() {
+function ui.error() {
   echo -e "${RED}[FAIL]${RESET} $*" >&2
 }
 
 # Print a warning message
-function print_warning() {
+function ui.print_warning() {
   echo -e "${YELLOW}[WARN]${RESET} $*" >&2
 }
