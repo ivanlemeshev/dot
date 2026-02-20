88d46d23bc839076da4447aa5d9ffc1ceb96e236
Ivan Lemeshev <vanlmshv@gmail.com>
2026-02-20 22:24:26 +0200

    Custom color scheme

--- .config/fish/conf.d/custom_theme.fish
+++ .config/fish/conf.d/custom_theme.fish
@@ -1,34 +1,31 @@
 # Custom theme for fish shell
 
 # --- Custom palette ---
-set -l bg0    191919
-set -l bg1    3d3839
-set -l bg5    3d3839
-set -l fg0    bbbbbb
-set -l fg1    bbbbbb
-set -l grey0  8e8e8e
-set -l grey1  8e8e8e
-set -l red    de6e7c
-set -l yellow b3a06a
-set -l green  819b69
-set -l aqua   66a5ad
-set -l blue   6099c0
-set -l purple b279a7
+set -l bg0     151515
+set -l bg1     505050
+set -l fg0     d0d0d0
+set -l fg1     f5f5f5
+set -l red     ac4142
+set -l yellow  f4bf75
+set -l green   90a959
+set -l blue    6a9fb5
+set -l magenta aa759f
+set -l cyan    75b5aa
 
 # --- Syntax highlighting ---
 set -g fish_color_normal         $fg0
-set -g fish_color_command        $green --bold
-set -g fish_color_keyword        $purple
+set -g fish_color_command        $green
+set -g fish_color_keyword        $magenta
 set -g fish_color_option         $green
 set -g fish_color_param          $fg1
 set -g fish_color_quote          $yellow
 set -g fish_color_redirection    $blue
 set -g fish_color_end            $green
 set -g fish_color_operator       $blue
-set -g fish_color_escape         $aqua
-set -g fish_color_comment        $grey0 --italics
+set -g fish_color_escape         $cyan
+set -g fish_color_comment        $bg1 --italics
 set -g fish_color_error          $red
-set -g fish_color_autosuggestion $grey0
+set -g fish_color_autosuggestion $bg1
 set -g fish_color_valid_path     --underline
 set -g fish_color_cancel         $red
 
@@ -45,8 +42,8 @@ set -g fish_color_host_remote $yellow
 set -g fish_color_status      $red
 
 # --- Pager (completions) ---
-set -g fish_pager_color_progress            $grey1
-set -g fish_pager_color_prefix              $yellow --bold
+set -g fish_pager_color_progress            $bg1
+set -g fish_pager_color_prefix              $yellow
 set -g fish_pager_color_completion          $fg0
-set -g fish_pager_color_description         $grey0
+set -g fish_pager_color_description         $bg1
 set -g fish_pager_color_selected_background --background=$bg1
