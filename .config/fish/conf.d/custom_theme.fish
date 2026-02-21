f6c7bc61f62de41bd4398a6863d18be6a45c20be
Ivan Lemeshev <vanlmshv@gmail.com>
2026-02-21 15:48:43 +0200

    Fix fish theme

--- .config/fish/conf.d/custom_theme.fish
+++ .config/fish/conf.d/custom_theme.fish
@@ -13,37 +13,37 @@ set -l magenta aa759f
 set -l cyan    75b5aa
 
 # --- Syntax highlighting ---
-set -g fish_color_normal         $fg0
-set -g fish_color_command        $green
-set -g fish_color_keyword        $magenta
-set -g fish_color_option         $green
-set -g fish_color_param          $fg1
-set -g fish_color_quote          $yellow
-set -g fish_color_redirection    $blue
-set -g fish_color_end            $green
-set -g fish_color_operator       $blue
-set -g fish_color_escape         $cyan
-set -g fish_color_comment        $bg1 --italics
-set -g fish_color_error          $red
-set -g fish_color_autosuggestion $bg1
-set -g fish_color_valid_path     --underline
-set -g fish_color_cancel         $red
+set -U fish_color_normal         $fg0
+set -U fish_color_command        $green
+set -U fish_color_keyword        $magenta
+set -U fish_color_option         $green
+set -U fish_color_param          $fg1
+set -U fish_color_quote          $yellow
+set -U fish_color_redirection    $blue
+set -U fish_color_end            $green
+set -U fish_color_operator       $blue
+set -U fish_color_escape         $cyan
+set -U fish_color_comment        $bg1 --italics
+set -U fish_color_error          $red
+set -U fish_color_autosuggestion $bg1
+set -U fish_color_valid_path     --underline
+set -U fish_color_cancel         $red
 
 # --- Selection & search ---
-set -g fish_color_selection    --background=$bg1
-set -g fish_color_search_match --background=$bg1
+set -U fish_color_selection    --background=$bg1
+set -U fish_color_search_match --background=$bg1
 
 # --- Prompt ---
-set -g fish_color_cwd         $blue
-set -g fish_color_cwd_root    $red
-set -g fish_color_user        $fg1
-set -g fish_color_host        $blue
-set -g fish_color_host_remote $yellow
-set -g fish_color_status      $red
+set -U fish_color_cwd         $blue
+set -U fish_color_cwd_root    $red
+set -U fish_color_user        $fg1
+set -U fish_color_host        $blue
+set -U fish_color_host_remote $yellow
+set -U fish_color_status      $red
 
 # --- Pager (completions) ---
-set -g fish_pager_color_progress            $bg1
-set -g fish_pager_color_prefix              $yellow
-set -g fish_pager_color_completion          $fg0
-set -g fish_pager_color_description         $bg1
-set -g fish_pager_color_selected_background --background=$bg1
+set -U fish_pager_color_progress            $bg1
+set -U fish_pager_color_prefix              $yellow
+set -U fish_pager_color_completion          $fg0
+set -U fish_pager_color_description         $bg1
+set -U fish_pager_color_selected_background --background=$bg1
