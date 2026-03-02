78a8432e9e1c590e9d535dc6008e4f2e2ed0586d
Ivan Lemeshev <vanlmshv@gmail.com>
2026-03-02 11:19:07 +0200

    Add git diff

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -28,6 +28,7 @@ return {
     "sindrets/diffview.nvim",
     -- https://github.com/sindrets/diffview.nvim
     -- Keep the plugin lazy-loaded until a diff command is used.
+    commit = "4516612fe98ff56ae0415a259ff6361a89419b0a",
     cmd = {
       "DiffviewOpen",
       "DiffviewFileHistory",
@@ -68,6 +69,51 @@ return {
           vim.cmd("DiffviewOpen " .. base .. "...HEAD")
         end,
         desc = "Git: PR diff vs upstream",
+      },
+    },
+  },
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    commit = "5ae580df72589f25b775ff2bdacfd7f7be8d63bd",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      picker = "telescope",
+      enable_builtin = true,
+    },
+    keys = {
+      {
+        "<leader>gl",
+        "<CMD>Octo issue list<CR>",
+        desc = "Octo: list GitHub issues",
+      },
+      {
+        "<leader>gp",
+        "<CMD>Octo pr list<CR>",
+        desc = "Octo: list GitHub pull requests",
+      },
+      {
+        "<leader>gd",
+        "<CMD>Octo discussion list<CR>",
+        desc = "Octo: list GitHub discussions",
+      },
+      {
+        "<leader>gn",
+        "<CMD>Octo notification list<CR>",
+        desc = "Octo: list GitHub notifications",
+      },
+      {
+        "<leader>gs",
+        function()
+          require("octo.utils").create_base_search_command({
+            include_current_repo = true,
+          })
+        end,
+        desc = "Octo: search GitHub issues and pull requests",
       },
     },
   },
