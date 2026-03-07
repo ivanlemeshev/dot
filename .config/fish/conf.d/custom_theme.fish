3fa7ee66010edc9b02a50514d30c68d7eb294d6d
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2026-03-07 14:25:09 +0200

    Fix fish theme (#420)

--- .config/fish/conf.d/custom_theme.fish
+++ .config/fish/conf.d/custom_theme.fish
@@ -25,7 +25,7 @@ set -Ux fish_color_operator       $blue
 set -Ux fish_color_escape         $cyan
 set -Ux fish_color_comment        $bg1 --italics
 set -Ux fish_color_error          $red
-set -Ux fish_color_autosuggestion $bg1
+set -Ux fish_color_autosuggestion $fg1 --dim --italics
 set -Ux fish_color_valid_path     --underline
 set -Ux fish_color_cancel         $red
 
@@ -45,5 +45,5 @@ set -Ux fish_color_status      $red
 set -Ux fish_pager_color_progress            $bg1
 set -Ux fish_pager_color_prefix              $yellow
 set -Ux fish_pager_color_completion          $fg0
-set -Ux fish_pager_color_description         $bg1
+set -Ux fish_pager_color_description         $fg1 --dim
 set -Ux fish_pager_color_selected_background --background=$bg1
